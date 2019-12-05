a2941049e75ac1f90612a455cfa460be12ee99a8
botsula <f2r0i0m0anko>
2019-12-05 15:32:08 +0200

    fixed1

--- src/main/java/json/JsonObject.java
+++ src/main/java/json/JsonObject.java
@@ -1,29 +1,33 @@
 package json;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Created by Andrii_Rodionov on 1/3/2017.
  */
 public class JsonObject extends Json {
 
-    public ArrayList sJson;
+    public ArrayList<JsonPair> theJson;
     public JsonObject(JsonPair... jsonPairs) {
+        this.theJson = new ArrayList<>();
+        Collections.addAll(theJson, jsonPairs);
     }
 
     @Override
     public String toJson() {
-        return sJson.toString();
-    }
-
-    public void addAll(JsonPair... jsonPairs){
-        for (JsonPair pair: jsonPairs) {
-            add(pair);
+        String fin = "";
+        int counter = 0;
+        for (JsonPair p : theJson) {
+            if (counter > 0) fin += ",";
+            fin += p.key + ":" + p.value.toJson();
+            counter += 1;
         }
+        return "{" + fin + "}";
     }
 
-    public void add(JsonPair jsonPair){
-        sJson.add(jsonPair);
+    public void add(JsonPair jsonPair) {
+        theJson.add(jsonPair);
     }
 
     public Json find(String name) {
